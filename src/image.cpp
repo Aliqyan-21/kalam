7e77af636704f1bdb4bb0870668d9544712f9ae2
Aliqyan-21 <aliqyanabid21@gmail.com>
2024-09-19 15:48:52 +0530

    feat: file extension abstraction

--- src/image.cpp
+++ src/image.cpp
@@ -3,13 +3,22 @@
 #include "save.h"
 
 namespace kalam {
-bool Image::load(const std::string &filepath) {
-  // we will implement loading image according to the file extension
-  return true;
+
+bool Image::load(const std::string& filepath) {
+    if (filepath.ends_with(".png")) {
+        return Load::loadPNG(filepath);
+    } else if (filepath.ends_with(".jpg") || filepath.ends_with(".jpeg")) {
+        return Load::loadJPEG(filepath);
+    }
+    return false;
 }
 
-bool Image::save(const std::string &filepath) {
-  // we will implement saving image according to the file extension
-  return true;
+bool Image::save(const std::string& filepath) {
+    if (filepath.ends_with(".png")) {
+        return Save::savePNG(filepath);
+    } else if (filepath.ends_with(".jpg") || filepath.ends_with(".jpeg")) {
+        return Save::saveJPEG(filepath);
+    }
+    return false;
 }
-} // namespace kalam
+}
