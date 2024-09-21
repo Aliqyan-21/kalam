d0320eab4d262e8e71a5c9dbe4f6bf0fdf9ee9bc
Aliqyan-21 <aliqyanabid21@gmail.com>
2024-09-21 13:41:21 +0530

    feat: show() function to show image stored in object according to os

--- include/image.h
+++ include/image.h
@@ -7,21 +7,22 @@
 namespace kalam {
 class Image {
 public:
-    bool load(const std::string& filepath);
-    bool save(const std::string& filepath);
+    bool load(const std::string &filepath);
+    bool save(const std::string &filepath);
+    void show() const;
+
+    void setData(int w, int h, int ch, std::vector<unsigned char>&& d);
 
     int getWidth() const { return width; }
     int getHeight() const { return height; }
     int getChannels() const { return channels; }
     const std::vector<unsigned char>& getData() const { return data; }
 
-    void setData(int w, int h, int ch, std::vector<unsigned char>&& d);
-
 private:
-    int width = 0;
-    int height = 0;
-    int channels = 0; 
-    std::vector<unsigned char> data; 
+    int width{0};
+    int height{0};
+    int channels{0};
+    std::vector<unsigned char> data;
 };
 } // namespace kalam
 
--- src/image.cpp
+++ src/image.cpp
@@ -1,6 +1,11 @@
 #include "image.h"
 #include "load.h"
 #include "save.h"
+#include <cstdlib>
+#include <iostream>
+#include <cstdio>
+#include <utility>
+#include <filesystem>
 
 namespace kalam {
 
@@ -27,6 +32,43 @@ void Image::setData(int w, int h, int ch, std::vector<unsigned char>&& d) {
     height = h;
     channels = ch;
     data = std::move(d);
+}
+
+void Image::show() const {
+    std::string temp_filepath;
+#ifdef _WIN32
+    char temp_name[L_tmpnam];
+    std::tmpnam(temp_name);
+    temp_filepath = std::string(temp_name) + ".png";  
+#elif __APPLE__ || __linux__
+    std::string temp_dir = std::filesystem::temp_directory_path();
+    temp_filepath = temp_dir + "/kalam_temp_image.png"; 
+#else
+    std::cerr << "Unsupported operating system for displaying the image." << std::endl;
+    return;
+#endif
+
+    if (!Save::savePNG(temp_filepath, *this)) {
+        std::cerr << "Failed to save the temporary image for display." << std::endl;
+        return;
+    }
+
+    std::string command;
+#ifdef _WIN32
+    command = "start " + temp_filepath;
+#elif __APPLE__
+    command = "open " + temp_filepath;
+#elif __linux__
+    command = "xdg-open " + temp_filepath;
+#endif
+
+    int result = std::system(command.c_str());
+    if (result != 0) {
+        std::cerr << "Failed to open the image with the default application." << std::endl;
+    }
+
+    // to delete the temp file that was created to show the image
+    std::filesystem::remove(temp_filepath);
 }
 
 } // namespace kalam
