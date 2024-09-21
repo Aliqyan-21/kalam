cd0ecac90cdaecabb8c9631e1dad7576ae7a526e
Aliqyan-21 <aliqyanabid21@gmail.com>
2024-09-22 01:24:21 +0530

    example code

--- examples/example_usage.cpp
+++ examples/example_usage.cpp
@@ -1,10 +1,18 @@
-#include "kalam.h"
 #include <iostream>
+#include <kalam.h>
 
 int main() {
   // this is how kalam will be used
-  kalam::Kalam app;
-  app.initialize();
-  std::cout << "Kalam library initialized." << std::endl;
+  kalam::Image img;
+  if (!img.load("pathtoimage/image.png")) {
+    std::cout << "Image not loaded" << std::endl;
+  } else {
+    std::cout << "Image is loaded" << std::endl;
+  }
+
+  std::cout << "Displaying the image..." << std::endl;
+
+  img.show();
+
   return 0;
 }
