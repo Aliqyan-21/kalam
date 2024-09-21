8004c7c6d0e7ebb8fa31c6909ca87fc8efedf340
Aliqyan-21 <aliqyanabid21@gmail.com>
2024-09-21 13:16:26 +0530

    tests

--- tests/test_image.cpp
+++ tests/test_image.cpp
@@ -1,9 +1,24 @@
 #include "image.h"
 #include <cassert>
+#include <iostream>
 
-int main() {
+int main(void) {
   kalam::Image img;
-  assert(img.load("test.png"));
-  assert(img.save("output.png"));
+  if (!img.load("testImages/dust.png")) {
+    std::cerr << "Failed to load PNG image" << std::endl;
+    return 1;
+  }
+  if (!img.save("testImages/opng.png")) {
+    std::cerr << "Failed to save PNG image" << std::endl;
+    return 1;
+  }
+  if (!img.load("testImages/test.jpg")) {
+    std::cerr << "Failed to load JPEG image" << std::endl;
+    return 1;
+  }
+  if (!img.save("testImages/ojpg.jpg")) {
+    std::cerr << "Failed to save JPEG image" << std::endl;
+    return 1;
+  }
   return 0;
 }
